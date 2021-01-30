70854a76c1e593b2fc06e792dc1cee691d83b19e
medsep <mehdi.sep@gmail.com>
2021-01-30 20:01:12 +1100

    V2

--- src/services/data.js
+++ src/services/data.js
@@ -16,7 +16,7 @@ const githubRepos = {
 		frontend: "https://github.com/sylviapap/racoon-frontend",
 		backend: "https://github.com/sylviapap/racoon-backend",
 	},
-	twitterClone: "https://github.com/sylviapap/twitter-clone",
+	socialApe: "https://github.com/medsep/Social-Media-App",
 	talkr: "https://github.com/sylviapap/TalkR",
 	subscription: "https://github.com/sylviapap/client-subscription-app",
 	whack: "https://github.com/sylviapap/whack-a-virus",
@@ -35,34 +35,18 @@ const liveSiteURLs = {
 };
 
 const blogURLs = {
-	personalSite:
-		"https://dev.to/sylviapap/how-to-make-your-own-no-template-personal-website-with-react-material-ui-and-netlify-pij",
-	bcrypt: "https://dev.to/sylviapap/bcrypt-explained-4k5c",
-	chatbot: "https://dev.to/sylviapap/make-a-simple-chatbot-with-javascript-1gc",
-	racoon:
-		"https://dev.to/sylviapap/check-out-my-coding-bootcamp-final-project-1lc1",
-	herokuConfig:
-		"https://dev.to/sylviapap/setting-heroku-config-vars-with-google-cloud-json-file-on-rails-4dnf",
-	swiftVapor:
-		"https://dev.to/sylviapap/learning-swift-ios-and-vapor-in-one-week-2ap7",
-	healthcareAI:
-		"https://dev.to/sylviapap/beginner-healthcare-ai-tensorflow-js-4ma1",
-	mapbox: "https://dev.to/sylviapap/mapbox-and-tool-tips-in-react-30ie",
-	whackVirus:
-		"https://dev.to/sylviapap/kill-coronavirus-with-this-simple-game-3l5",
-	ruby:
-		"https://dev.to/sylviapap/the-ruby-enumerable-operator-and-max-vs-maxby-ac6",
+	
 };
 
 export const projects = [
 	{
 		title: "Social Media App",
 		imageURL: "twitter-clone.jpg",
-		websiteURL: liveSiteURLs.twitterClone,
+		websiteURL: liveSiteURLs.socialApe,
 		description:
 			"Clone project to answer application question 'Tell your life story in Tweets'",
 		technology: "React • JavaScript • CSS • HTML • Material UI",
-		githubURL: githubRepos.twitterClone,
+		githubURL: githubRepos.socialApe,
 	}
 ];
 
@@ -130,12 +114,7 @@ export const contactItems = [
 		icon: "fab fa-dev",
 		text: "Blog",
 	},
-	{
-		href: data.twitter,
-		icon: "fa fa-twitter",
-		text: "Twitter",
-	},
-	{
+		{
 		href: data.stackOverflow,
 		icon: "fab fa-stack-overflow",
 		text: "Stack Overflow",
@@ -145,42 +124,6 @@ export const contactItems = [
 export const blogPosts = [
 	{
 		href: blogURLs.personalSite,
-		title: "Personal Website with React, Material UI, & Netlify",
-	},
-	{
-		href: blogURLs.bcrypt,
-		title: "BCrypt Explained",
-	},
-	{
-		href: blogURLs.chatbot,
-		title: "JavaScript Chatbot",
-	},
-	{
-		href: blogURLs.racoon,
-		title: "COVID Symptom Checker & Map",
-	},
-	{
-		href: blogURLs.herokuConfig,
-		title: "Heroku Configuration",
-	},
-	{
-		href: blogURLs.swiftVapor,
-		title: "Swift, iOS, & Vapor",
-	},
-	{
-		href: blogURLs.healthcareAI,
-		title: "Healthcare AI & TensorFlow",
-	},
-	{
-		href: blogURLs.mapbox,
-		title: "Mapbox & Tool Tips in React",
-	},
-	{
-		href: blogURLs.whackVirus,
-		title: "Whack-a-Mole + Coronavirus",
-	},
-	{
-		href: blogURLs.ruby,
-		title: "Comparisons in Ruby Using <=>",
-	},
+		title: "Blog posts to appear here",
+	}
 ];
